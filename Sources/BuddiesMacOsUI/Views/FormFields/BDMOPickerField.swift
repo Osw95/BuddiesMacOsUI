912c50cc332ba190afc2940e73648dc5a872179e
Oswaldo Ferral Mejía <1114538@onuriscp.com>
2025-03-28 11:15:57 -0600

    [UPDATE]: New generic picker

---
+++ Sources/BuddiesMacOsUI/Views/FormFields/BDMOPickerField.swift
@@ -0,0 +1,46 @@
+//
+//  BDMOPickerField.swift
+//  BuddiesMacOsUI
+//
+//  Created by Oswaldo Ferral Mejia on 28/03/25.
+//
+
+public struct BDMOPickerField <T: Identifiable & Hashable>: View {
+    
+    private var strTextLabel: String
+    private var arrTextTag: [T]
+    @Binding var anyBindingValue: T
+
+    public init(strTextLabel: String, anyBindingValue: Binding<T>, arrTextTag: [T]) {
+        self.strTextLabel = strTextLabel
+        self.arrTextTag = arrTextTag
+        _anyBindingValue = anyBindingValue
+    }
+    
+    public var body: some View {
+        
+        HStack {
+            Text("\(strTextLabel)")
+                .font(.headline)
+            Spacer()
+            Picker("", selection: $anyBindingValue) {
+                ForEach(arrTextTag) { item in
+                    getTextTagView(item)
+                }
+            }
+            .pickerStyle(.menu)
+            .labelsHidden()
+            .fixedSize()
+        }
+    }
+    
+    @ViewBuilder
+    private func getTextTagView(_ item: T) -> some View {
+        if let textDisplay = item as? String {
+            Text(textDisplay).tag(item as T)
+        } else {
+            Text("No valid value")
+        }
+       
+    }
+}
