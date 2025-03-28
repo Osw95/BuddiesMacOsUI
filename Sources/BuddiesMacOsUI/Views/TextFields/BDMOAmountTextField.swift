acb724f2d1956610c589ea661cc5e94c38a8303a
Oswaldo Ferral Mejía <1114538@onuriscp.com>
2025-03-27 19:50:00 -0600

    [UPDATE]: Textfield amount change

--- Sources/BuddiesMacOsUI/Views/TextFields/BDMOAmountTextField.swift
+++ Sources/BuddiesMacOsUI/Views/TextFields/BDMOAmountTextField.swift
@@ -9,6 +9,7 @@ public struct BDMOAmountTextField: View {
     @Binding public var strText: String
     private var strTextLabel: String
     private var strPrompt: String
+    @FocusState var bIsFocused: Bool
     
     public init(strText: Binding<String>, strTextLabel: String, strPrompt: String = "$0.00") {
         _strText = strText
@@ -22,9 +23,16 @@ public struct BDMOAmountTextField: View {
                 .font(.headline)
             TextField("", text: $strText , prompt: Text(strPrompt))
                 .onChange(of: strText) { oldValue, newValue in
-                    strText = newValue.localizedCurrencyFormatted() ?? ""
+                    if !bIsFocused {
+                        strText = newValue.localizedCurrencyFormatted() ?? ""
+                    }
                 }
-                .customTextfield()
+                .background(bIsFocused ? .white.opacity(1) : .white.opacity(0.0))
+                .overlay(RoundedRectangle(cornerRadius: 8)
+                    .stroke(bIsFocused ? Color.blue : Color.gray.opacity(0), lineWidth: 1.5))
+                .focused($bIsFocused)
+                .animation(.easeInOut(duration: 0.2), value: bIsFocused)
+                .modifier(GeneralCurrencyTextFieldModifier())
                 .autocorrectionDisabled()
         }
     }
